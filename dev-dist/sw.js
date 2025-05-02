c98a792d6cebf17dff9f7da25b442704efde52bd
RamC Venkatasamy <ramc.venkatasamy@e14s.com>
2025-05-02 16:38:09 -0500

    fixing the vercel issue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6n8don0nsb"
+    "revision": "0.4hp58fhlu0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
